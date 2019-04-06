cebc891c6b866f5aa786e357ce73dbda6ccc588f
Liron Saedi <lionsaedi@gmail.com>
2019-04-05 17:30:46 -0700

    Today I started to do the Add First function of the singly linked list

---
+++ SimplyLinkedList/LinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplyLinkedList
+{
+    public class LinkedList<T>
+    {
+        internal class Node
+        {
+            public T Value;
+            public Node Next;
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public Node(T value, Node next)
+            {
+                Value = value;
+                Next = next;
+            }
+        }
+
+        int Count = 0;
+        Node Head;
+
+        void AddFirst(T value)
+        {
+
+            if (Head == null)
+            {
+                new Node(value);
+            }
+            T swapValue = Head.Value;
+
+
+        }
+
+
+    }
+}
